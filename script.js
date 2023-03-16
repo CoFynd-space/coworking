8b3c53ea5e80af1a42b269b32b757dee351bce2c
CoFynd-space <127395689+CoFynd-space@users.noreply.github.com>
2023-03-16 07:48:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ var img = document.querySelectorAll(".myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 var span = document.getElementsByClassName("close")[0];
-function imageScale(event) {
-  modal.style.display = "block";
-  modalImg.src = event.target.src;
-  console.log(event.target.src);
-}
 span.onclick = function () {
   modal.style.display = "none";
 };
+const mql = window.matchMedia("(max-width: 480px)");
+const imageScale  = (event) => {
+  if (mql.matches) {
+    modal.style.display = "block";
+    modalImg.src = event.target.src;
+  } else {
+    modal.style.display = "none";
+  }
+}
+mql.addEventListener("click", imageScale);
