9e146f136e5905690ee75dd9734d4a293b22c628
CoFynd-space <127395689+CoFynd-space@users.noreply.github.com>
2023-04-04 15:09:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ const imageScale  = (event) => {
   }
 }
 mql.addEventListener("click", imageScale);
+
+// function sendMail(){
+//   Email.send({
+//     Host : "smtp.elasticemail.com",
+//     Username : "shivammaheshwari503@gmail.com",
+//     Password : "93B9A0E01851828946348DADB87A9DE493F4",
+//     To : 'shivam8077990774@gmail.com',
+//     From : "shivammaheshwari503@gmail.com",
+//     Subject : "This is the subject",
+//     Body : "Name:" + document.getElementById("name").value
+//     + "<br> Email:" + document.getElementById("email").value
+//     + "<br> Message:" + document.getElementById("message").value
+// }).then(
+//   message => alert(message + " " + "Message Send Successfully!")
+// );
+
+// }
